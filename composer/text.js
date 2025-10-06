acaa4b98d938713d904c0e254bf706f785e8a0d9
Whsxg <shabanov.matveyka@bk.ru>
2025-10-06 23:19:28 +0300

    Update text.js

--- composer/text.js
+++ composer/text.js
@@ -3,15 +3,20 @@ const { Composer, Markup } = require('telegraf');
 const composer = new Composer();
 
 composer.start(async (ctx) => {
+    const photoUrl = 'https://github.com/MatveyVue/giftcap/blob/main/HateCaps.jpeg?raw=true';
     const buttons = Markup.inlineKeyboard([
         [Markup.button.url('Go App', 'https://t.me/HateCapsBot/Hatecaps')],
         [Markup.button.url('Channel', 'https://t.me/@whsxg0')],
     ]);
 
-    return ctx.reply(
-        `Hi friend, here you can take part in the NFT Hate Caps draw, which will be in great demand for their uniqueness.`,
-        { parse_mode: 'HTML', reply_markup: buttons.reply_markup } 
-    );
+    const messageText = `Hi friend, here you can take part in the NFT Hate Caps draw, which will be in great demand for their uniqueness.`;
+
+    // 4. Используем ctx.replyWithPhoto
+    return ctx.replyWithPhoto(photoUrl, {
+        caption: messageText, 
+        parse_mode: 'HTML',  
+        ...buttons            
+    });
 });
 
 module.exports = composer;
