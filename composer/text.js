f8335e5c710e445976ecd566fd85fe62ffae40e9
Whsxg <shabanov.matveyka@bk.ru>
2025-10-06 23:13:56 +0300

    Update text.js

--- composer/text.js
+++ composer/text.js
@@ -4,15 +4,14 @@ const composer = new Composer();
 
 composer.start(async (ctx) => {
     const buttons = Markup.inlineKeyboard([
-        [
-            [Markup.button.url('Go App', 'https://t.me/HateCapsBot/Hatecaps')],
-            [Markup.button.url('Channel', 'https://t.me/@whsxg0')],
-        ]
+        [Markup.button.url('Go App', 'https://t.me/HateCapsBot/Hatecaps')],
+        [Markup.button.url('Channel', 'https://t.me/@whsxg0')],
     ]);
 
     return ctx.reply(
-        `Hi friend, here you can take part in the NFT Hate Caps draw, which will be in great demand for their uniqueness`,
-        { parse_mode: 'HTML', ...buttons }
+        `Hi friend, here you can take part in the NFT Hate Caps draw, which will be in great demand for their uniqueness.`,
+        { parse_mode: 'HTML', reply_markup: buttons.reply_markup } 
     );
 });
+
 module.exports = composer;
